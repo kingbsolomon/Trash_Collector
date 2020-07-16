5aaefc5c9033053c8c6a6b10f6a9b3359faa1149
Brent Solomon <bjsolomon@gmail.com>
2020-07-16 01:13:55 -0400

    Added Customer Model Class

---
+++ Trash Collector/Models/Customer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trash_Collector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public char MiddleInit { get; set; }
+        [Required, Phone]
+        public string PhoneNumber { get; set; }
+        [Required, EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required, MaxLength(2)]
+        public string State { get; set; }
+        [Required, MaxLength(5)]
+        public int ZipCode { get; set; }
+        public float Longtitude { get; set; }
+        public float Latitude { get; set; }
+        [CreditCard]
+        public int CreditCard { get; set; }
+        
+    }
+}
